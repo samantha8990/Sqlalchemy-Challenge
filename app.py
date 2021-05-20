05ed092a5d3bc4f83bf82823f81ab9f2fc0a6944
Samantha Sommer <samanthajeddy@gmail.com>
2021-05-20 13:16:48 -0700

    hw

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ from flask import Flask, jsonify
 
 
 #Database Setup
-engine = create_engine('sqlite:///Resources/hawaii.sqlite')
+engine = create_engine('sqlite:///hawaii.sqlite')
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 measurement = Base.classes.measurement
 station = Base.classes.station
+
+session=Session(engine)
 
 #Flask Setup
 app = Flask(__name__)
@@ -33,8 +35,27 @@ def home():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    prc=session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= '2016-08-23').order_by(Measurement.date. desc()).all()
+    return jsonify(prc)
 
 
+@app.route("/api/v1.0/stations")
+def stations():
+    station_list=session.query(Station.station).all()
+    return jsonify(station_list)
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    last_year=session.query(Measurement.station, Measurement.date, Measurement.tobs).\
+    filter(Measurement.station=='USC00519281').\
+    filter(Measurement.date >= '2016-08-23').\
+    order_by(Measurement.date).all()
+    return jsonify(tobs)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>") 
+def   
+    
+# 4. Define main behavior
 if __name__ == "__main__":
     app.run(debug=True)
